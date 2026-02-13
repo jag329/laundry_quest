9e374abb450ac1de485291959ecb4ae2c0a13cb2
James Goodwin <jag329@lehigh.edu>
2026-02-13 16:33:40 -0500

    Laundry Text commit

--- src/game/LaundryBasket.ts
+++ src/game/LaundryBasket.ts
@@ -1,22 +1,29 @@
-import {Actor, BoxBody, AppearanceComponent, FilledBox, StateEvent, StateManagerComponent, Passive, RoleComponent, Scene, InertMovement, MovementComponent, GestureHandlers, ManualMovement} from "../jetlag";
+import { Actor, BoxBody, AppearanceComponent, FilledBox, StateEvent, StateManagerComponent, Passive, RoleComponent, Scene, InertMovement, MovementComponent, GestureHandlers, ManualMovement, TextSprite } from "../jetlag";
 
 /**
  * Object to implement a laundry basket. 
  */
 export class LaundryBasket extends Actor {
-    static basketWidth: number = 10;
-    static basketHeight: number = 5;
+    static basketWidth: number = 1;
+    static basketHeight: number = 0.5;
 
     public constructor(color: string, cx: number, cy: number) {
         super({
             //for now the appearance will be a 
-            appearance: new FilledBox({
-                width: LaundryBasket.basketWidth,
-                height: LaundryBasket.basketHeight,
-                fillColor: color
-            }),
+            appearance: [
+                new FilledBox({
+                    width: LaundryBasket.basketWidth,
+                    height: LaundryBasket.basketHeight,
+                    fillColor: color
+                }),
+                new TextSprite({
+                    center: true,
+                    face: "Arial",
+                    color: "#fffff",
+                    size: 12
+                }, "Laundry Basket or something")],
             rigidBody: new BoxBody({
-                cx: cx, 
+                cx: cx,
                 cy: cy,
                 width: LaundryBasket.basketWidth,
                 height: LaundryBasket.basketHeight
